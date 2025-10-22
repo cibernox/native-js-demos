2794159a7fff540682a16eccf88ea48d7c3aec78
Miguel Camba <miguel.camba@gmail.com>
2025-10-22 14:05:27 +0200

    Add basic svelte app

---
+++ svelte/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+  let count = 0;
+
+  function increment() {
+    console.log('### Incrementing count');
+    count += 1;
+  }
+
+  function decrement() {
+    console.log('### Decrementing count');
+    count -= 1;
+  }
+
+  setTimeout(increment, 1000);
+</script>
+<hstack>
+  <button onclick={decrement}>Decrement</button>
+  <text>{count}!!!</text>
+  <button onclick={increment}>Increment</button>
+</hstack>
